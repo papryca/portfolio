176fc1fbc3d197bca179bae154fd31f9e6800626
inessa <inessasmorgunova@gmail.com>
2024-04-15 21:25:35 -0400

    feat: add adaptivity

--- src/app/components/about-me.tsx
+++ src/app/components/about-me.tsx
@@ -4,7 +4,7 @@ import cards from "@/app/components/card/card-tabs";
 const AboutMe =() => {
   return (
     <div className=' bg-gray-50'>
-      <div className='h-96 pt-10 grid grid-rows-1 items-end mx-10 h-auto'>
+      <div className=' pt-10 grid grid-rows-1 items-end mx-10 h-auto'>
         <div>
           <h2 className='text-3xl underline decoration-pink pb-5'>My Portfolio</h2>
         </div>
