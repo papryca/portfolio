e341f7582733d9e0e412807bfe6486be250ebbc1
inessa <inessasmorgunova@gmail.com>
2024-04-15 23:15:40 -0400

    fix: change size

--- src/app/components/about-me.tsx
+++ src/app/components/about-me.tsx
@@ -6,7 +6,7 @@ const AboutMe =() => {
     <div className=' bg-gray-50'>
       <div className=' pt-10 grid grid-rows-1 items-end mx-10 h-auto'>
         <div>
-          <h2 className='text-3xl underline decoration-pink pb-5'>My Portfolio</h2>
+          <h2 className='text-3xl underline decoration-yellow pb-5'>My Portfolio</h2>
         </div>
         <div className=' flex items-center gap-10 flex-col py-10'>
           {cards.map(card =>( <Card card={card} key={card.index}/>))}
